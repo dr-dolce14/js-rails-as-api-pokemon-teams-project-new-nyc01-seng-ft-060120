80d29f4c0df4349b38700b1a3b4be05646cec372
travisprol0 <travisjohnprol@gmail.com>
2020-07-20 16:32:53 -0400

    half of pokemon list append

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,8 @@
 class PokemonsController < ApplicationController
+
+    def index
+        pokemons = Pokemon.all
+
+        render json: pokemons
+    end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,43 +1,67 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
-const mainTag = document.querySelector('main')
+    const BASE_URL = "http://localhost:3000"
+    const TRAINERS_URL = `${BASE_URL}/trainers`
+    const POKEMONS_URL = `${BASE_URL}/pokemons`
+    const mainTag = document.querySelector('main')
+    
+    function fetchTrainers() {
+        fetch(TRAINERS_URL)
+        .then(response => response.json())
+        .then(results => {
+            results.forEach(trainer => renderTrainer(trainer)) 
+        })
+    }
+    
+    function renderTrainer(trainer) {
+        const trainerDiv = document.createElement('div')
+        const pokemonList = document.createElement('ul')
 
+        trainerDiv.className = "card"
+        trainerDiv.dataset.id = trainer.id
+        let p = document.createElement('p')
+        p.innerText = trainer.name   
+        let button = document.createElement('button')
+        button.innerText = "Add Pokemon"
+        button.dataset.trainerId = trainer.id
+        trainerDiv.append(p)
+        trainerDiv.append(button)
+        mainTag.append(trainerDiv)
+        trainerDiv.append(pokemonList)
+        fetchPokemon()
+    }
+    
+    function fetchPokemon() {
+        fetch(POKEMONS_URL)
+        .then(response => response.json())
+        .then(results => {
+            results.forEach(pokemon => createListItem(pokemon))
+        })
+    }   
+    const pokeTrainerDiv = document.getElementsByClassName("card")
+    console.log(pokeTrainerDiv.dataset)
+    
+    function createListItem(pokemon) {
+        const listItem = document.createElement('li')
+        const releaseButton = document.createElement('button')
+        listItem.innerText = pokemon.nickname.concat("(", pokemon.species).concat(")" )
+        listItem.className = pokemon.trainer_id
+        releaseButton.className = "release"
+        releaseButton.dataset.id = pokemon.id
+        listItem.append(releaseButton)
 
-function fetchTrainers() {
-    fetch(TRAINERS_URL)
-    .then(response => response.json())
-    .then(results => {
-        results.forEach(trainer => renderTrainer(trainer))
+        if (pokeTrainerDiv === pokeTrainerDiv.dataset) {
+            pokeTrainerDiv.append(listItem)
+        }
+            // console.log(listItem)
+    }
+
+    function appendListItem(pokemon){
         
-    })
-}
-fetchTrainers()
+    }
 
-function renderTrainer(trainer) {
-    const trainerDiv = document.createElement('div')
-    trainerDiv.className = "card"
-    trainerDiv.dataset.id = trainer.id
-    let p = document.createElement('p')
-    p.innerText = trainer.name   
-    let button = document.createElement('button')
-    button.innerText = "Add Pokemon"
-    button.dataset.trainer = trainer.id
-    trainerDiv.append(p)
-    trainerDiv.append(button)
-    mainTag.append(trainerDiv)
-    
-    
-// √ assign target element 'main' to a variable 
-// √ create new div with the class of 'card' and data set id of the trainer
-// √ set innerHTML to all the stuff that's required for trainer
-// √ append div to main
+    // if trainer ID of a pokemon matches the trainer ID of the div
 
-}
-
-
-
+    fetchTrainers()
 }) // end of DOMContendLoaded
